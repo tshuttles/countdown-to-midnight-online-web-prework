2a05b9bfdbfd39d714d186e3878746da4524c649
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-09 17:49:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = 0
+  while counter < number
+    number = 10
+    puts "#{counter} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num_secs)
+  sleep 5 
 end
